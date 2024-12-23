2396cfed0ef8ff0e3a2c3ad3dae95754f39e8d9c
Ombra <carterolsen2211@gmail.com>
2024-12-23 01:29:35 -0500

    Update lib.rs

--- EoE-program/scr/lib.rs
+++ EoE-program/scr/lib.rs
@@ -1,1 +1,30 @@
+use anchor_lang::prelude::*;
+use instructions::*;
 
+pub mod errors;
+pub mod instructions;
+pub mod states;
+
+declare_id!("11111111111111111111111111111111");
+
+#[cfg(target_os = "solana")]
+#[global_allocator]
+static ALLOC: smalloc::Smalloc<
+    { anchor_lang::solana_program::entrypoint::HEAP_START_ADDRESS as usize },
+    { anchor_lang::solana_program::entrypoint::HEAP_LENGTH as usize },
+    16,
+    1024,
+> = smalloc::Smalloc::new();
+
+
+#[program]
+pub mod qshield {
+    use super::*;
+
+    pub fn init_vault_accounts(
+        ctx: Context<InitVaultAccounts>, args: VerifyArgs
+    ) -> Result<()> {
+        instructions::init_vault_accounts(ctx, args)
+    }
+    
+}
