e0e2454de8209f74d67a058b4f88ff4cde37fc35
Ombra <carterolsen2211@gmail.com>
2024-12-23 01:29:08 -0500

    Update errors.rs

--- EoE-program/scr/errors.rs
+++ EoE-program/scr/errors.rs
@@ -1,1 +1,30 @@
+use anchor_lang::error_code;
+
+#[error_code]
+pub enum TerritoryError {
+    AlreadyInitialized,
+    NotInitialized,
+    InvalidPosition
+}
+
+#[error_code]
+pub enum VampireError {
+    AlreadyInitialized,
+    NotInitialized,
+    AlreadyHunting,
+    InvalidLoot
+}
+
+#[error_code]
+pub enum CraftError {
+    AlreadyInitialized,
+    NotInitialized,
+    InvalidID
+}
+#[error_code]
+pub enum PlayerError {
+    AlreadyInitialized,
+    NotInitialized,
+    NotEnoughCurrencies
+}
 
