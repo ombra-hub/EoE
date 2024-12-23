ff0f9c638c40c0862449809a2f17a48bb8607479
Ombra <carterolsen2211@gmail.com>
2024-12-23 01:29:59 -0500

    Update vault.rs

--- EoE-program/scr/states/vault.rs
+++ EoE-program/scr/states/vault.rs
@@ -1,1 +1,24 @@
+use anchor_lang::prelude::*;
+use crate::errors::PlayerError;
 
+#[account]
+pub struct Vault {
+    owner_pubkey: Pubkey,
+    is_initialized: bool,
+}
+
+impl Vault {
+
+    // Set to maximum account size to leave expansion room, find what it is
+    pub const MAXIMUM_SIZE: usize = 5000;
+
+    pub fn init(&mut self, owner_pubkey: Pubkey) -> Result<()> {
+        require_eq!(self.is_initialized, false, PlayerError::AlreadyInitialized);
+
+        self.owner_pubkey = owner_pubkey;
+
+        self.is_initialized = true;
+
+        Ok(())
+    }
+}
